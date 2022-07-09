aff27c26c35899cf60eb2ca3187c6277f9e387d3
zitsko <92641284+zitsko@users.noreply.github.com>
2022-07-09 20:54:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let passwordLength = document.getElementById("psw-length")
+const passwordLength = document.getElementById("psw-length")
 const generatedPasswordOne = document.getElementById("psw-one")
 const generatedPasswordTwo = document.getElementById("psw-two")
-let errorTextEl = document.getElementById("error-text")
+
 
 document.getElementById("gen-btn").addEventListener("click",generateRandomPassword)
 
